afbd7bd358857e7517dc88771fa09c7542b6c0c4
Brian Chiu <brianchiu33@gmail.com>
2023-10-11 19:41:37 -0400

    add userRouter endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import quizRouter from "./routers/quizRouter";
 import customCoursesRouter from "./routers/customCoursesRouter";
 import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
 
 const app = express();
 const port = 8080;
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 
 app.use("/quiz", quizRouter);
 app.use("/customCourse", customCoursesRouter);
+app.use("/user", userRouter);
 
 app.listen(port, () => {
   return console.log(

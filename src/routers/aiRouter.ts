8dbb98509ecc01dfbdd8f552afe308a699204e3a
Devanshu Sabnani <devanshusabnani2611@gmail.com>
2023-10-15 19:51:31 -0400

    Updated AiRouter

--- src/routers/aiRouter.ts
+++ src/routers/aiRouter.ts
@@ -4,15 +4,29 @@ const aiRouter = express.Router();
 
 aiRouter.post('/analyze-image', async (req, res) => {
   try {
-    const { imageUrl } = req.body;
+    const { image_url } = req.body;
 
-    const aiResponse = await axios.post('http://localhost:8082/predict', { imageUrl });
-    
-    // todo: prcoess the returned data if  needed
+    // image_url is required
+    if (!image_url) {
+      return res.status(400).json({ error: 'image_url is required' });
+    }
 
+    //sending post request
+    const aiResponse = await axios.post('http://127.0.0.1:8082/predict', { image_url });
+
+    //Response recieved
     return res.status(200).json(aiResponse.data);
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Error:', error.message);
+    if (error.response) {
+      console.error('Response data:', error.response.data);
+      console.error('Response status:', error.response.status);
+    } else if (error.request) {
+      console.error('No response received:', error.request);
+    } else {
+      console.error('Error', error.message);
+    }
+
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 });

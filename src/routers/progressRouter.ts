f05ed833169aedbef26202dbe24ccafd471bb822
coralg <coralg@sheridancollege.ca>
2024-01-30 03:40:54 -0500

    added WIP logic for the progress when the user starts a course, lesson and gesture

--- src/routers/progressRouter.ts
+++ src/routers/progressRouter.ts
@@ -2,6 +2,7 @@ import express from "express";
 import prisma from "../database/prisma";
 import { Request, Response } from "express";
 import { authenticateUser } from "../auth/authenticateUser";
+import lessonRouter from "./LessonRouter";
 const progressRouter = express.Router();
 
 progressRouter.post("/startedALesson", async (req, res) => {
@@ -16,6 +17,7 @@ progressRouter.post("/startedALesson", async (req, res) => {
 
     let startedCourseId;
     let startedLessonId;
+    let startedGestureId;
 
     // user has clicked on a lesson and now we have to "Start the lesson"
     // ie, identify the lesson started and add the lesson to the started lesson list
@@ -32,6 +34,10 @@ progressRouter.post("/startedALesson", async (req, res) => {
                     },
                 ],
             },
+            include: {
+                user: true,
+                startedLessons: true,
+            }
         });
 
         if (!course) {
@@ -51,16 +57,6 @@ progressRouter.post("/startedALesson", async (req, res) => {
 
             startedCourseId = courseResult.id;
 
-        // check if the lesson exists
-        // const lesson = await prisma.startedLesson.findFirst({
-        //     where : {
-        //         AND: [
-        //             {lessonId: lessonId as string}, // makes sure its the lesson we're seeing
-        //             {userId: user.id} // makes sure this is the use that is logged in
-        //         ]
-        //     }
-        // });
-        
             // create a new lesson
             const lessonResult = await prisma.startedLesson.create({
                 data: {
@@ -84,13 +80,135 @@ progressRouter.post("/startedALesson", async (req, res) => {
                     gestureId,
                     lessonId,
                     userId: user.id,
-                    
+                    courseId,
                     startedLessonId,
                 },
                 include: {
                     lesson: true,
                 },
             });
+
+            return res.status(200).json("started a course lesson and a sign");
+        } else {
+            // COURSE DOES EXIST
+
+            // check if the lesson exists
+            const lesson = await prisma.startedLesson.findFirst({
+            where : {
+                AND: [
+                    {lessonId: lessonId as string}, // makes sure its the lesson we're seeing
+                    {userId: user.id} // makes sure this is the use that is logged in
+                    ]
+                },
+                include: {
+                    course: true,
+                    startedGestures: true,
+                }
+            });
+
+            // LESSON DOESN'T EXIST
+            // make a lesson and add it to the existing course's startedLesson list
+            // ie. the user has started this course (and one lesson) but not this one  
+            if (!lesson) {
+                const lessonResult = await prisma.startedLesson.create({
+                    data: {
+                        courseId,
+                        lessonId,
+                        userId: user.id,
+                        maxReachedGesture: 0,
+                        startedCourseId: course.id,
+                    },
+                    include: {
+                        course: true,
+                        startedGestures: true,
+                    },
+                });
+
+                // add the lesson to the startedLessons list
+                course.startedLessons.push(lesson);
+
+                startedLessonId = lessonResult.id;
+
+                // if the lesson doesn't exist remember the gesture DOESN'T EXIST EITHER
+
+                // create a new gesture
+                const gestureResult = await prisma.startedGesture.create({
+                    data: {
+                        gestureId,
+                        lessonId,
+                        userId: user.id,
+                        courseId,
+                        startedLessonId,
+                    },
+                    include: {
+                        lesson: true,
+                    },
+                });
+
+                // add the new gesture to the list of started gestures
+                lessonResult.startedGestures.push(gestureResult);
+
+                return res.status(200).json("started a lesson and a gesture");
+            } else {
+            // lesson DOES exist
+            // ie. user has started this course and this lesson so now move onto gesture 
+
+            // check if the gesture exists
+            const gesture = await prisma.startedGesture.findFirst({
+                where : {
+                    AND: [
+                        {gestureId: gestureId as string}, // makes sure its the gesture we're seeing
+                        {userId: user.id}, // makes sure this is the use that is logged in
+                        ]
+                    }
+                });
+
+            // GESTURE DOESN'T EXIST
+            // ie. the user has started this course and this lesson but not this gesture
+            // so make a gesture and add it to the existing lesson's startedGesture list
+
+            if (!gesture){
+                const gestureResult = await prisma.startedGesture.create({
+                    data: {
+                        gestureId,
+                        lessonId,
+                        userId: user.id,
+                        courseId,
+                        startedLessonId: lesson.id,
+                    },
+                    include: {
+                        lesson: true,
+                    },
+                });
+
+                // add the gesture to the startedGestures list
+                lesson.startedGestures.push(gestureResult);
+
+                return res.status(200).json("started a new gesture");
+            } else {
+            // GESTURE DOES EXIST 
+            // ie. user has started this course, this lesson and this gesture so now move onto next lesson or finish course
+            
+            // find the real lesson 
+            lesson.lessonId 
+            const realLesson = await prisma.lesson.findUnique({
+                where: {
+                    id: lesson.lessonId,
+                },
+                include: {
+                    gestures: true,
+                },
+            });
+
+            // YI AYUDA
+            if (realLesson.gestures.length > lesson.startedGestures.length){
+                // go to next gesture there's still more gestures
+            } else {
+                // finish lesson 
+
+            }
+            }
+            }
         }
             // return res.status(404).json({ error: "Lesson does not exist" });
         } catch (error) {
@@ -99,6 +217,5 @@ progressRouter.post("/startedALesson", async (req, res) => {
         }
    
 });
-
 
 progressRouter.post("/startedAGesture");
